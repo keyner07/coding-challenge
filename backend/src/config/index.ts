40dd1bb5c67875860404a7c4f94d0b39795901dd
keyner07 <47069469+keyner07@users.noreply.github.com>
2024-06-06 14:07:52 -0400

    environment variables

---
+++ backend/src/config/index.ts
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const HOST_DB = process.env.HOST_DB;
+export const DATABASE_DB = process.env.DATABASE_DB;
+export const USERNAME_DB = process.env.USERNAME_DB;
+export const PASSWORD_DB = process.env.PASSWORD_DB;
